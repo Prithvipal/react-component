6edc66cef52d79fa35b9d7a01d117afc019d41c7
Prithvipal Singh <prithvirathore99@gmail.com>
2020-11-28 13:31:15 +0530

    component reuse: ApprovalCard

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail  from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () =>{
     
     return (
       <div className="ui container comments">
-        <CommentDetail
-          author="Prithvi"
-          timeAgo="Today at 4:34 PM"
-          content="Nice blog post!"
-          avatar={faker.image.people()}
-        />
+        <ApprovalCard>
+            <CommentDetail
+            author="Prithvi"
+            timeAgo="Today at 4:34 PM"
+            content="Nice blog post!"
+            avatar={faker.image.people()}
+            />
+        </ApprovalCard>
         <CommentDetail
           author="Parth"
           timeAgo="Today at 6:47 PM"
