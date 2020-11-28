b4a809b6290c125d7d7d883502939807f24cb9a3
Prithvipal Singh <prithvirathore99@gmail.com>
2020-11-28 12:03:14 +0530

    passing props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 import CommentDetail  from './CommentDetail'
 
 const App = () =>{
     
     return (
-        <div className="ui container comments">
-           <CommentDetail author="Prithvi"/> 
-           <CommentDetail author="Parth"/> 
-           <CommentDetail author="Rajani"/>
-        </div>
+      <div className="ui container comments">
+        <CommentDetail
+          author="Prithvi"
+          timeAgo="Today at 4:34 PM"
+          content="Nice blog post!"
+          avatar={faker.image.people()}
+        />
+        <CommentDetail
+          author="Parth"
+          timeAgo="Today at 6:47 PM"
+          content="I like the subject"
+          avatar={faker.image.people()}
+        />
+        <CommentDetail
+          author="Rajani"
+          timeAgo="Yesterdat at 11:00 AM"
+          content="I like the writing"
+          avatar={faker.image.people()}
+        />
+      </div>
     );
 }
 
